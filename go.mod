cb56c66fb0ed8dc2fc7f85a186a51cb0fce30391
Hasan Yousef <hyousef@msn.com>
2022-02-15 11:42:14 +0300

    alternat e sentences

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.io/hajsf/erp
 
 go 1.18
 
-require github.com/lib/pq v1.10.4
+require github.com/lib/pq v1.10.2
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os/exec"
 
 	_ "github.com/lib/pq"
 )
@@ -10,14 +12,30 @@ import (
 const (
 	host     = "localhost"
 	port     = 5432
-	user     = "postgres"
-	password = "<password>"
-	dbname   = "<dbname>"
+	user     = "hasan.y"
+	password = "Dana0Yara"
+	dbname   = "postgres"
 )
 
 func main() {
+	cmd := exec.Command("powershell", "pg_ctl", "stop") // stop the pgsql server
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd = exec.Command("powershell", "pg_ctl", "start") // start up the pgsql server
+
+	err = cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// connection string
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	println(psqlconn)
 
 	// open database
 	db, err := sql.Open("postgres", psqlconn)
@@ -31,6 +49,66 @@ func main() {
 	CheckError(err)
 
 	fmt.Println("Connected!")
+
+	sql := "CREATE TABLE IF NOT EXISTS people(id SERIAL PRIMARY KEY, name VARCHAR, salary INTEGER);"
+	_, err = db.Exec(sql)
+	if err != nil {
+		fmt.Println("err1: ", err)
+		panic(err)
+	}
+
+	// sql = "INSERT INTO people(name, salary) VALUES (@name, @salary);" // VALUES ($1, $2);
+	//_, err = db.Exec(sql, "Joe", 10000)
+	// https://www.calhoun.io/inserting-records-into-a-postgresql-database-with-gos-database-sql-package/
+	// https://kb.objectrocket.com/postgresql/how-to-insert-record-in-postgresql-database-using-go-database-sql-package-785
+
+	sqlStatement, err := db.Prepare("INSERT INTO people(name, salary) VALUES ($1, $2) RETURNING id;")
+	if err != nil {
+		fmt.Println("errx: ", err)
+		log.Fatal(err)
+	}
+	defer sqlStatement.Close()
+	idx := 0
+	err = sqlStatement.QueryRow("Karam", 30000).Scan(&idx)
+	if err != nil {
+		fmt.Println("erry: ", err)
+		log.Fatal(err)
+	}
+	fmt.Println("New record ID is:", idx)
+
+	/*	sqlStatement := "INSERT INTO people(name, salary) VALUES ($1, $2) RETURNING id;"
+
+		err = db.QueryRow(sqlStatement, "jon@calhoun.io", 300).Scan(&idx)
+		if err != nil {
+			fmt.Println("err2: ", err)
+			panic(err)
+		}
+		fmt.Println("New record ID is:", idx)
+	*/
+	sqlQuery := `SELECT * FROM people`
+	rows, err := db.Query(sqlQuery)
+	if err != nil {
+		fmt.Println("err3: ", err)
+		panic(err)
+	}
+	// https://docs.immudb.io/1.0.0/jumpstart.html#sql-operations-with-the-go-sdk
+	var (
+		id     int
+		name   string
+		salary int
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name, &salary)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name, salary)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func CheckError(err error) {
